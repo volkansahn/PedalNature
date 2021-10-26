9b311e9a34c9807a8bb86bf0b4f192f3c88df284
Volkan Sahin <vsahin0985@gmail.com>
2021-10-26 10:58:17 +0300

    Change Route Info Design

--- PedalNature/Views/Cells/CreateRoutePost/CreateRouteInfoTableViewCell.swift
+++ PedalNature/Views/Cells/CreateRoutePost/CreateRouteInfoTableViewCell.swift
@@ -13,10 +13,9 @@ final class CreateRouteInfoTableViewCell: UITableViewCell {
     
     private let durationHeaderLabel : UILabel = {
         let label = UILabel()
-        label.textColor = .label
-        label.font = .systemFont(ofSize: 15, weight: .semibold)
+        label.textColor = .systemBackground
+        label.font = .systemFont(ofSize: 20, weight: .semibold)
         label.textAlignment = .center
-        label.backgroundColor = .lightGray
         label.text = "Duration"
         label.numberOfLines = 1
         return label
@@ -24,10 +23,9 @@ final class CreateRouteInfoTableViewCell: UITableViewCell {
     
     private let distanceHeaderLabel : UILabel = {
         let label = UILabel()
-        label.textColor = .label
-        label.font = .systemFont(ofSize: 15, weight: .semibold)
+        label.textColor = .systemBackground
+        label.font = .systemFont(ofSize: 20, weight: .semibold)
         label.textAlignment = .center
-        label.backgroundColor = .lightGray
         label.text = "Distance"
         label.numberOfLines = 1
         return label
@@ -35,10 +33,9 @@ final class CreateRouteInfoTableViewCell: UITableViewCell {
     
     private let maxSpeedHeaderLabel : UILabel = {
         let label = UILabel()
-        label.textColor = .label
-        label.font = .systemFont(ofSize: 15, weight: .semibold)
+        label.textColor = .systemBackground
+        label.font = .systemFont(ofSize: 20, weight: .semibold)
         label.textAlignment = .center
-        label.backgroundColor = .lightGray
         label.text = "Max.Speed"
         label.numberOfLines = 1
         return label
@@ -46,10 +43,9 @@ final class CreateRouteInfoTableViewCell: UITableViewCell {
     
     private let maxElevationHeaderLabel : UILabel = {
         let label = UILabel()
-        label.textColor = .label
-        label.font = .systemFont(ofSize: 15, weight: .semibold)
+        label.textColor = .systemBackground
+        label.font = .systemFont(ofSize: 20, weight: .semibold)
         label.textAlignment = .center
-        label.backgroundColor = .lightGray
         label.text = "Max.Elevation"
         label.numberOfLines = 1
         return label
@@ -57,10 +53,9 @@ final class CreateRouteInfoTableViewCell: UITableViewCell {
     
     private let durationLabel : UILabel = {
         let label = UILabel()
-        label.textColor = .label
-        label.font = .systemFont(ofSize: 17, weight: .semibold)
+        label.textColor = .systemBackground
+        label.font = .systemFont(ofSize: 24, weight: .semibold)
         label.textAlignment = .center
-        label.backgroundColor = .lightGray
         label.text = "1h30d"
         label.numberOfLines = 1
         return label
@@ -68,10 +63,9 @@ final class CreateRouteInfoTableViewCell: UITableViewCell {
     
     private let distanceLabel : UILabel = {
         let label = UILabel()
-        label.textColor = .label
-        label.font = .systemFont(ofSize: 17, weight: .semibold)
+        label.textColor = .systemBackground
+        label.font = .systemFont(ofSize: 24, weight: .semibold)
         label.textAlignment = .center
-        label.backgroundColor = .lightGray
         label.text = "1.37km"
         label.numberOfLines = 1
         return label
@@ -79,10 +73,9 @@ final class CreateRouteInfoTableViewCell: UITableViewCell {
     
     private let maxSpeedLabel : UILabel = {
         let label = UILabel()
-        label.textColor = .label
-        label.font = .systemFont(ofSize: 17, weight: .semibold)
+        label.textColor = .systemBackground
+        label.font = .systemFont(ofSize: 24, weight: .semibold)
         label.textAlignment = .center
-        label.backgroundColor = .lightGray
         label.text = "22m/sn"
         label.numberOfLines = 1
         return label
@@ -90,18 +83,105 @@ final class CreateRouteInfoTableViewCell: UITableViewCell {
     
     private let maxElevationLabel : UILabel = {
         let label = UILabel()
-        label.textColor = .label
-        label.font = .systemFont(ofSize: 17, weight: .semibold)
+        label.textColor = .systemBackground
+        label.font = .systemFont(ofSize: 24, weight: .semibold)
         label.textAlignment = .center
-        label.backgroundColor = .lightGray
         label.text = "750m"
         label.numberOfLines = 1
         return label
     }()
+	
+	private let durationContainerView : UIView = {
+		let view = UIView()
+		view.backgroundColor = .label
+		view.layer.shadowRadius = 8
+		view.layer.shadowOffset = CGSize(width: 3, height: 3)
+		view.layer.shadowOpacity = 0.5
+		view.layer.cornerRadius = 20
+		view.translatesAutoresizingMaskIntoConstraints = false
+		return view
+	}()
+	
+	private let durationContainerContentsLayer : UIView = {
+		let view = UIView()
+		view.backgroundColor = .label
+		view.layer.cornerRadius = 20
+		view.layer.masksToBound = true
+		view.translatesAutoresizingMaskIntoConstraints = false
+		return view
+	}()
+	
+	private let distanceContainerView : UIView = {
+		llet view = UIView()
+		view.backgroundColor = .label
+		view.layer.shadowRadius = 8
+		view.layer.shadowOffset = CGSize(width: 3, height: 3)
+		view.layer.shadowOpacity = 0.5
+		view.layer.cornerRadius = 20
+		view.translatesAutoresizingMaskIntoConstraints = false
+		return view
+	}()
+	
+	private let distanceContainerContentsLayer : UIView = {
+		let view = UIView()
+		view.backgroundColor = .label
+		view.layer.cornerRadius = 20
+		view.layer.masksToBound = true
+		view.translatesAutoresizingMaskIntoConstraints = false
+		return view
+	}()
+	
+	private let maxEleContainerView : UIView = {
+		let view = UIView()
+		view.backgroundColor = .label
+		view.layer.shadowRadius = 8
+		view.layer.shadowOffset = CGSize(width: 3, height: 3)
+		view.layer.shadowOpacity = 0.5
+		view.layer.cornerRadius = 20
+		view.translatesAutoresizingMaskIntoConstraints = false
+		return view
+	}()
+	
+	private let maxEleContainerContentsLayer : UIView = {
+		let view = UIView()
+		view.backgroundColor = .label
+		view.layer.cornerRadius = 20
+		view.layer.masksToBound = true
+		view.translatesAutoresizingMaskIntoConstraints = false
+		return view
+	}()
+	
+	private let maxSpeedContainerView : UIView = {
+		let view = UIView()
+		view.backgroundColor = .label
+		view.layer.shadowRadius = 8
+		view.layer.shadowOffset = CGSize(width: 3, height: 3)
+		view.layer.shadowOpacity = 0.5
+		view.layer.cornerRadius = 20
+		view.translatesAutoresizingMaskIntoConstraints = false
+		return view
+	}()
+	
+	private let maxSpeedContainerContentsLayer : UIView = {
+		let view = UIView()
+		view.backgroundColor = .label
+		view.layer.cornerRadius = 20
+		view.layer.masksToBound = true
+		view.translatesAutoresizingMaskIntoConstraints = false
+		return view
+	}()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?){
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        contentView.addSubview(durationHeaderLabel)
+        contentView.addSubview(durationContainerView)
+		contentView.addSubview(durationContainerContentsLayer)
+		contentView.addSubview(distanceContainerView)
+		contentView.addSubview(distanceContainerContentsLayer)
+		contentView.addSubview(maxEleContainerView)
+		contentView.addSubview(maxEleContainerContentsLayer)
+		contentView.addSubview(maxSpeedContainerView)
+		contentView.addSubview(maxSpeedContainerContentsLayer)
+		contentView.addSubview(durationHeaderLabel)
         contentView.addSubview(distanceHeaderLabel)
         contentView.addSubview(maxSpeedHeaderLabel)
         contentView.addSubview(maxElevationHeaderLabel)
@@ -118,45 +198,71 @@ final class CreateRouteInfoTableViewCell: UITableViewCell {
     override func layoutSubviews() {
         super.layoutSubviews()
         
-        durationHeaderLabel.frame = CGRect(x: 0,
-                                           y: 0,
-                                           width: contentView.width/4-10,
-                                           height: contentView.height/3)
-        
-        distanceHeaderLabel.frame = CGRect(x: durationHeaderLabel.right,
-                                           y: 0,
-                                           width: contentView.width/4,
-                                           height: contentView.height/3)
-        
-        maxSpeedHeaderLabel.frame = CGRect(x: distanceHeaderLabel.right,
-                                           y: 0,
-                                           width: contentView.width/4,
-                                           height: contentView.height/3)
-        
-        maxElevationHeaderLabel.frame = CGRect(x: maxSpeedHeaderLabel.right,
-                                               y: 0,
-                                               width: contentView.width/4+10,
-                                               height: contentView.height/3)
-        
-        durationLabel.frame = CGRect(x: 0,
+		let containerSize = (contentView.width - 60)/2
+		
+        durationContainerView.frame = CGRect(x: 20,
+                                           y: 20,
+                                           width: containerSize,
+                                           height: containerSize)
+		durationContainerContentsLayer.frame = durationContainerView.bounds
+		
+		durationHeaderLabel.frame = CGRect(x: durationContainerContentsLayer.left,
+                                           y: durationContainerContentsLayer.top,
+                                           width: containerSize,
+                                           height: containerSize/2)
+		
+		durationLabel.frame = CGRect(x: durationContainerContentsLayer.left,
                                      y: durationHeaderLabel.bottom,
-                                     width: contentView.width/4-10,
-                                     height: 2*contentView.height/3)
+                                     width: containerSize,
+                                     height: containerSize/2)
         
-        distanceLabel.frame = CGRect(x: durationLabel.right,
+        distanceContainerView.frame = CGRect(x: durationContainerContentsLayer.right + 20,
+                                           y: 20,
+                                           width: containerSize,
+                                           height: containerSize)
+		distanceContainerContentsLayer.frame = distanceContainerView.bounds
+		
+		distanceHeaderLabel.frame = CGRect(x: distanceContainerContentsLayer.left,
+                                           y: distanceContainerContentsLayer.bottom,
+                                           width: containerSize,
+                                           height: containerSize/2)
+        
+        distanceLabel.frame = CGRect(x: distanceContainerContentsLayer.left,
                                      y: distanceHeaderLabel.bottom,
-                                     width: contentView.width/4,
-                                     height: 2*contentView.height/3)
+                                     width: containerSize,
+                                     height: containerSize/2)
+		
+		maxSpeedContainerView.frame = CGRect(x: 20,
+                                           y: durationContainerView.bottom + 20,
+                                           width: containerSize,
+                                           height: containerSize)
+		maxSpeedContainerContentsLayer.frame = maxSpeedContainerView.bounds
+		
+		maxSpeedHeaderLabel.frame = CGRect(x: maxSpeedContainerContentsLayer.left,
+                                           y: maxSpeedContainerContentsLayer.bottom,
+                                           width: containerSize,
+                                           height: containerSize/2)
         
-        maxSpeedLabel.frame = CGRect(x: distanceLabel.right,
+        maxSpeedLabel.frame = CGRect(x: maxSpeedContainerContentsLayer.left,
                                      y: maxSpeedHeaderLabel.bottom,
-                                     width: contentView.width/4,
-                                     height: 2*contentView.height/3)
+                                     width: containerSize,
+                                     height: containerSize/2)
+		
+		maxEleContainerView.frame = CGRect(x: maxSpeedContainerContentsLayer.right + 20,
+                                           y: distanceContainerView.bottom + 20,
+                                           width: containerSize,
+                                           height: containerSize)
+		maxEleContainerContentsLayer.frame = maxEleContainerView.bounds
+		
+		maxElevationHeaderLabel.frame = CGRect(x: maxEleContainerContentsLayer.left,
+                                               y: maxEleContainerContentsLayer.bottom,
+                                               width: containerSize,
+                                               height: containerSize/2)
         
-        maxElevationLabel.frame = CGRect(x: maxSpeedLabel.right,
+        maxElevationLabel.frame = CGRect(x: maxEleContainerContentsLayer.left,
                                          y: maxElevationHeaderLabel.bottom,
-                                         width: contentView.width/4+10,
-                                         height: 2*contentView.height/3)
+                                         width: containerSize,
+                                         height: containerSize/2)
     }
     
     public func configure(duration : String, distance : String, maxElevation : String, maxSpeed: String){
