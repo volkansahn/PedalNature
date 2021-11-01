ca4ab2d9db2dbca5f73ca932de1c3debc7ecd64c
Volkan <vsahin0985@gmail.com>
2021-11-01 23:10:57 +0300

    Animation

---
+++ PedalNature/Controllers/Other/General/PlayerView.swift
@@ -0,0 +1,20 @@
+//
+//  PlayerView.swift
+//  PedalNature
+//
+//  Created by Volkan on 31.10.2021.
+//
+
+import UIKit
+
+class PlayerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ PedalNature/Controllers/Other/General/VideoPlayer.swift
@@ -0,0 +1,12 @@
+//
+//  VideoPlayer.swift
+//  PedalNature
+//
+//  Created by Volkan on 31.10.2021.
+//
+
+import UIKit
+
+class VideoPlayer: NSObject {
+
+}
