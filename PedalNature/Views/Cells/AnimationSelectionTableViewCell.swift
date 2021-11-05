db27e8b85ff59fa55c74122a05c84a1222aa6316
Volkan <vsahin0985@gmail.com>
2021-11-05 21:47:37 +0300

    New Version

---
+++ PedalNature/Views/Cells/AnimationSelectionTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AnimationSelectionTableViewCell.swift
+//  PedalNature
+//
+//  Created by Volkan on 4.11.2021.
+//
+
+import UIKit
+
+class AnimationSelectionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
